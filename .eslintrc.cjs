cf6618d81343b284c4a220a259510775bd26de05
bartektricks <bartektricks@gmail.com>
2023-04-09 18:02:51 +0200

    Project setup.

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDit: __dirname,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    '@typescript-eslint/triple-slash-reference': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+  },
+};
